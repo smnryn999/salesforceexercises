3af0c2fc2116576f08a46d0eaa898310634c184a
smnryn999 <96185425+smnryn999@users.noreply.github.com>
2022-10-31 17:47:44 +0300

    Update testA1.js

--- testA1.js
+++ testA1.js
@@ -79,7 +79,7 @@ submit.onclick = function () {
 // PASS BUTTON
 pass.onclick = function () {
    if(n==test1.length-1) {
-      explanation.innerHTML = "Test Completed" + ". Correct answers: " + corrects + ", wrong answers: " + wrongs;
+      explanation.innerHTML = `Test Completed. Total questions: ${test.length}, correct answers: ${corrects}, wrong answers: ${wrongs}`;
       explanation.style.color = "navy";
    }else {
       n++;
