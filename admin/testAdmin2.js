9ac12b6a14e3e8f513e01daecce760c224a04b97
smnryn999 <96185425+smnryn999@users.noreply.github.com>
2023-01-28 16:54:41 +0300

    Update testAdmin2.js

--- admin/testAdmin2.js
+++ admin/testAdmin2.js
@@ -1,1 +1,153 @@
+var bullets = document.getElementsByClassName("bullet");
+var buttons = document.querySelectorAll("button");
+var explanation = document.querySelector("#explanation");
+var question = document.getElementById("question");
+var options = document.getElementsByClassName("option");
+var optionContents = document.getElementsByClassName("optionContent");
+var index = Number(document.getElementById("index").innerHTML);
+var back = document.getElementById("back");
+var reset = document.getElementById("reset");
+var submit = document.querySelector("#submit");
+var pass = document.querySelector("#pass");
+const A = bullets[0], B = bullets[1], C = bullets[2], D = bullets[3];
+var i=0, n=0, m=0;
+var wrongs = 0, corrects = 0;
 
+// QUESTIONS AND OPTIONS CONTENTS
+const test1 = [""];
+const options1 = [[]];
+const answers1 = [];
+
+document.querySelector("#numberOfQuestion").innerHTML = test1.length;
+
+question.innerHTML = test1[n];
+
+for(opt of optionContents){
+   opt.innerHTML = options1[m][i];
+   i++;
+}
+
+// SELECTING OPTIONS
+for(opt of options){
+   opt.onclick =function () {
+      for(b of bullets){
+         b.style.backgroundColor = "Azure";
+         b.style.color = "black";
+         b.parentElement.style.backgroundColor = "azure";
+      }
+      this.children[0].style.backgroundColor = "DodgerBlue";
+      this.children[0].style.color = "white";
+      this.style.backgroundColor = "#fafafa";
+   }
+}
+
+// SUBMIT BUTTON
+submit.onclick = function () {
+   var bool = A.style.color != "white" && B.style.color != "white" && C.style.color != "white" && D.style.color != "white";
+   if(bool) {
+      alert("Please select an option or click pass button");
+   }
+   else {
+      if(answers1[n].style.backgroundColor == "dodgerblue") {
+         explanation.innerHTML = "Correct Answer";
+         explanation.style.color = "green";
+         corrects++;
+         }
+      else if(answers1[n].style.backgroundColor == "green") {
+         null;
+      }
+      else {
+         explanation.innerHTML = "Wrong Answer";
+         explanation.style.color = "red";
+         wrongs++;
+         for(b of bullets){
+            if(b.style.backgroundColor == "dodgerblue" && b != answers1[n]) {
+               b.style.backgroundColor = "red";
+            }
+         }
+      }
+      
+      answers1[n].style.color = "white";
+      answers1[n].style.backgroundColor = "green";      
+   }
+}
+
+// PASS BUTTON
+pass.onclick = function () {
+   if(n==test1.length-1) {
+      explanation.innerHTML = `Test Completed. Total questions: ${test1.length}, correct answers: ${corrects}, wrong answers: ${wrongs}`;
+      explanation.style.color = "navy";
+   }else {
+      n++;
+      question.innerHTML = test1[n];
+   
+      i=0;
+      m++;
+      for(opt of optionContents){
+      opt.innerHTML = options1[m][i];
+      i++;
+      }
+      
+      if(options1[m].length == 2) {
+         options[2].style.display = "none";
+         options[3].style.display = "none";
+      }
+      else if(options1[m].length == 3) {
+         options[3].style.display = "none";
+      }
+      else {
+         for(op of options){
+            op.style.display = "block";
+         }
+      }
+   }
+   
+   if(index==test1.length) {return;}
+   index++;
+   document.getElementById("index").innerHTML = index;
+   
+   for(o of options){
+      o.style.backgroundColor = "Azure";
+      o.style.color = "black";
+      o.children[0].style.backgroundColor = "azure";
+      o.children[0].style.color = "black";
+      }
+   explanation.innerHTML = null;
+   
+}
+// RESET BUTTON
+reset.onclick = function () {
+   n = 0;
+   m = 0;
+   i = 0;
+   wrongs = 0;
+   corrects = 0;
+   index = 1;
+   document.getElementById("index").innerHTML = index;
+   question.innerHTML = test1[n];
+   for(opt of optionContents){
+      opt.innerHTML = options1[m][i];
+      i++;
+   }
+      
+   for(o of options){
+      o.style.backgroundColor = "Azure";
+      o.style.color = "black";
+      o.children[0].style.backgroundColor = "azure";
+      o.children[0].style.color = "black";
+      }
+   explanation.innerHTML = null;
+   
+   if(options1[m].length == 2) {
+         options[2].style.display = "none";
+         options[3].style.display = "none";
+      }
+   else if(options1[m].length == 3) {
+         options[3].style.display = "none";
+      }
+   else {
+         for(op of options){
+            op.style.display = "block";
+         }
+      }
+}
