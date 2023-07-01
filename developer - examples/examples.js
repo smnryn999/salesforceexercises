5833399da65f0d2ed68d8203de4ba0045fbe8e72
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-07-01 20:57:05 +0300

    Update examples.js

--- developer - examples/examples.js
+++ developer - examples/examples.js
@@ -19,3 +19,12 @@ var showButtons = document.querySelectorAll(".showSolution");
    for(let i=0; i<showButtons.length; i++){
       document.querySelectorAll(".counter")[i].innerHTML = i+1;
    }
+
+   var pres = document.querySelectorAll("pre");
+   for(var i=0; i<pres.length; i++){
+      var content = pres[i].innerHTML;
+      content = content.replaceAll("new", "<var>new</var>");
+      content = content.replaceAll("System.debug", "<span class='key'>System.debug</span>");
+      content = content.replaceAll("String", "<span class='datatype'>String</span>");
+      document.querySelectorAll("pre")[i].innerHTML = content;
+   }
