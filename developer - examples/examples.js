6f5007bc0b840c4794675858ecd91eb8d36e1ce4
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-07-04 20:51:12 +0300

    Update examples.js

--- developer - examples/examples.js
+++ developer - examples/examples.js
@@ -28,14 +28,19 @@ var showButtons = document.querySelectorAll(".showSolution");
       var content = pres[i].innerHTML;
       content = content.replaceAll("new", "<var>new</var>");
       content = content.replaceAll("System.debug", "<span class='key'>System.debug</span>");
-      content = content.replaceAll("String", "<span class='datatype'>String</span>");
-      content = content.replaceAll(/boolean/gi, "<span class='datatype'>Boolean</span>");
+     // content = content.replaceAll("String", "<span class='datatype'>String</span>");
+     // content = content.replaceAll(/boolean/gi, "<span class='datatype'>Boolean</span>");
       
       content = content.replaceAll(/[0-9]/g, function (x) {return `<span class='number'>${x}</span>`;});
       
       content = content.replaceAll(/List<string>/gi, "<span class='datatype'>List&#60;String&#62;</span>");
-      content = content.replaceAll(/string[]/gi, "<span class='datatype'>String[]</span>");
-      content = content.replaceAll(/Set<string>/gi, "<span class='datatype'>Set&#60;String&#62;</span>");      
+      content = content.replaceAll(/Set<string>/gi, "<span class='datatype'>Set&#60;String&#62;</span>");     
+      
+      var datatypes = ["String", "Date", "Integer", "Boolean", "Decimal", "Datetime", "Double"]; 
+      for(d of datatypes){
+         // var re = new RegExp(d,"ig");
+         content = content.replaceAll(d, "<span class='datatype'>"+d+"</span>");
+      } 
       
       var punctuation = ["(", ")", "{", "}", "[", "]", ","];
       for(p of punctuation){
