8dd391ede3d1b76b71ba79d7ee0fb1e4acde7532
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-12 01:29:58 +0300

    Update testAdmin3.js

--- admin/testAdmin3.js
+++ admin/testAdmin3.js
@@ -14,16 +14,18 @@ var i=0, n=0, m=0;
 var wrongs = 0, corrects = 0;
 
 // QUESTIONS AND OPTIONS CONTENTS
-const test = ["What is Salesforce?", "What is CRM (Customer Relationship Management)?", "What is a cloud-based system?", "If we cannot delete a field, what could be the reason?", "If we want to make a field required universally and only for some profiles, how do we do it?",
-              "Which field type cannot be used as External ID?"];
+const test = ["What is Salesforce?", "What is CRM (Customer Relationship Management)?", "What is a cloud-based system?", "To be able to use Salesforce, we need to install it on our device. True or false?",
+              "Why are objects used in Salesforce?",
+              "If we cannot delete a field, what could be the reason?", "If we want to make a field required universally and only for some profiles, how do we do it?", "Which field type cannot be used as External ID?"];
 
 const options1 = [["A popular social media platform.","A cloud-based customer relationship management (CRM) software.","A mobile operating system.","A programming language for web development."],
                  ["A system for tracking customer interactions and managing relationships.","A tool for managing employee performance.","A cloud-based data storage solution.","A type of email marketing software."],
                  ["A computer system that relies on local hardware for processing.","A system that stores data on physical storage devices within an organization's premises.","A technology that uses virtualized resources accessible via the internet.","A network of interconnected physical servers managed by a company."],
+                 ["True","False"], ["To store and manage data for specific business entities.","To define user roles and permissions.","To create visual interfaces for user interactions.","To automate business processes using workflows."],
                  ["It may have been used in a formula field.","It may be a master-detail relationship field in an object whose data is used by a roll-up summary field.","It may be a standard field.","All of above."],
                  ["We mark required checkbox in the field settings.","We make the field required on the page layout.","We make the field required using a validation rule.","None of above. Because, this cannot be performed."],
                  ["Text","Number","Email","URL"]];
-const answers = [B,A,C,D,C,D];
+const answers = [B,A,C,B,A,D,C,D];
 
 document.querySelector("#numberOfQuestion").innerHTML = test.length;
 
