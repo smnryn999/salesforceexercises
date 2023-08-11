102c54b827e55168840806078d257d4dca8b049f
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-11 18:09:35 +0300

    Update testD2.js

--- developer/testD2.js
+++ developer/testD2.js
@@ -15,11 +15,12 @@ var wrongs = 0, corrects = 0;
 
 // QUESTIONS AND OPTIONS CONTENTS
 const test = ['Which of the following Visualforce code snippets correctly creates a command button that calls the "updateRecord" method in the controller when clicked?', 'What does the "trigger.new" keyword refer to in Salesforce Apex triggers?',
-             "What is the term used to describe the ability of a subclass to inherit attributes and methods from its superclass?", "What is the correct syntax for declaring a variable of type Integer and initializing it to the value 10?"];
+             "What is the term used to describe the ability of a subclass to inherit attributes and methods from its superclass?", "What is the correct syntax for declaring a variable of type Integer and initializing it to the value 10?",
+             'Which of the following Apex code snippets correctly initializes a List of Strings with three elements: "Apple", "Banana", and "Cherry"'];
 
 const options1 = [['<code>&lt;apex:commandButton action="{!updateRecord}" value="Update"/&gt;</code>','<code>&lt;apex:commandButton method="updateRecord" value="Update"/&gt;</code>','<code>&lt;apex:commandButton onclick="{!updateRecord}" value="Update"/&gt;</code>','<code>&lt;apex:commandButton actionName="updateRecord" value="Update"/&gt;</code>'],
                  ["It represents the old values of records before they were updated.","It refers to the new values of records after they were updated.","It represents the values of records before they were deleted.","It's used to access the values of records in a related object."], ["Overloading","Polymorphism","Inheritance","Abstraction"]
-                 ["Integer i = 10;","Int i = 10;","i = Integer(10);","Integer i; i = 10;"]];
+                 ["Integer i = 10;","Int i = 10;","i = Integer(10);","Integer i; i = 10;"], ["<code>List<String> fruits = new List<String>(); <br> fruits.add('Apple'); <br> fruits.add('Banana'); <br> fruits.add('Cherry');</code>","<code>List<Text> fruits = new List<Text>(); <br> fruits.add('Apple'); <br> fruits.add('Banana'); <br> fruits.add('Cherry');</code>",'List<String> fruits = new List<String>{ "Apple", "Banana", "Cherry" };',"List<String> fruits = new List<String>{'Apple', 'Banana', 'Cherry'};"]];
 const answers = [A,B,C,A];
 
 document.querySelector("#numberOfQuestion").innerHTML = test.length;
