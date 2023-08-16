bd8c0a07ee66a9f21df5e003165e402112fe531b
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-16 14:52:00 +0300

    Update testAdmin4.js

--- testAdmin4.js
+++ testAdmin4.js
@@ -14,11 +14,12 @@ var i=0, n=0, m=0;
 var wrongs = 0, corrects = 0;
 
 // QUESTIONS AND OPTIONS CONTENTS
-const test = ["What is the purpose of a Salesforce search layout?", "How are many-to-many relationships implemented in Salesforce?"]
+const test = ["What is the purpose of a Salesforce search layout?", "How are many-to-many relationships implemented in Salesforce?","How many fields can be assigned to a compact layout?","Which of the following is used to set highlights panel."]
 
 const options1 = [["To customize the layout of record detail pages.","To manage user permissions and access.","To define the layout of dashboard components.","To define the fields displayed in search results."],
-                 ["By using external objects to link records from different objects.","By creating a custom junction object to connect two objects.","By using triggers to directly link records between objects.","By enabling the 'Many-to-Many' option in the object settings."]];
-const answers = [D,B];
+                 ["By using external objects to link records from different objects.","By creating a custom junction object to connect two objects.","By using triggers to directly link records between objects.","By enabling the 'Many-to-Many' option in the object settings."],
+                 [3,5,7,10], ["Compact Layout","Page Layout","Search Layout","List View Layout"]];
+const answers = [D,B,D,A];
 
 document.querySelector("#numberOfQuestion").innerHTML = test.length;
 
