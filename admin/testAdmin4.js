13d3f6980b6bf92aed93a222ba0a851daa7929c4
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-17 23:45:11 +0300

    Update testAdmin4.js

--- admin/testAdmin4.js
+++ admin/testAdmin4.js
@@ -14,9 +14,9 @@ var i=0, n=0, m=0;
 var wrongs = 0, corrects = 0;
 
 // QUESTIONS AND OPTIONS CONTENTS
-const test = ["What is the purpose of a Salesforce search layout?", "How are many-to-many relationships implemented in Salesforce?", "How many fields can be assigned to a compact layout?",
-             "What is the primary purpose of a Salesforce Dashboard?", "Compact layouts can be assigned to profiles or individual users. True or false?", "If a user's profile has full CRUD permissions for an object but cannot edit some fields of the records, what could be the reason?",
-             "What defines a one-to-many relationship in Salesforce?", "Only one case assignment rule can be active at a time. True or false?", "Which dashboard component type should we use when we have a single value that we want to show within a range of custom values?"];
+const test = ["What is the purpose of a search layout?", "How are many-to-many relationships implemented in Salesforce?", "How many fields can be assigned to a compact layout?", "What is the primary purpose of a Salesforce Dashboard?",
+              "Compact layouts can be assigned to profiles or individual users. True or false?", "If a user's profile has full CRUD permissions for an object but cannot edit some fields of the records, what could be the reason?",
+              "What defines a one-to-many relationship in Salesforce?", "Only one case assignment rule can be active at a time. True or false?", "Which dashboard component type should we use when we have a single value that we want to show within a range of custom values?"];
 
 const options1 = [["To customize the layout of record detail pages.","To manage user permissions and access.","To define the layout of dashboard components.","To define the fields displayed in search results."],
                  ["By using external objects to link records from different objects.","By creating a custom junction object to connect two objects.","By using triggers to directly link records between objects.","By enabling the 'Many-to-Many' option in the object settings."],
