d7b17d621086e7503ae4b3d5ea4b24a49cc3d006
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-17 23:22:00 +0300

    Update testAdmin4.js

--- admin/testAdmin4.js
+++ admin/testAdmin4.js
@@ -16,14 +16,15 @@ var wrongs = 0, corrects = 0;
 // QUESTIONS AND OPTIONS CONTENTS
 const test = ["What is the purpose of a Salesforce search layout?", "How are many-to-many relationships implemented in Salesforce?","How many fields can be assigned to a compact layout?",
              "What is the primary purpose of a Salesforce Dashboard?", "Compact layouts can be assigned to profiles or individual users. True or false?", "If a user's profile has full CRUD permissions for an object but cannot edit some fields of the records, what could be the reason?",
-             "What defines a one-to-many relationship in Salesforce?",""];
+             "What defines a one-to-many relationship in Salesforce?", "Only one case assignment rule can be active at a time. True or false?"];
 
 const options1 = [["To customize the layout of record detail pages.","To manage user permissions and access.","To define the layout of dashboard components.","To define the fields displayed in search results."],
                  ["By using external objects to link records from different objects.","By creating a custom junction object to connect two objects.","By using triggers to directly link records between objects.","By enabling the 'Many-to-Many' option in the object settings."],
                  [3,5,7,10], ["To automate data entry tasks.","To create and manage user accounts.","To display visual representations of data from reports.","To synchronize data with external systems."], ["True","False"],
                  ["The fields may be read-only in the page layout.","Edit permission may not be granted in field-level security.","The profile license the user has may have been insufficient.","A and B"],
-                 ["Creating a custom junction object to link two objects.","Using external objects to establish a connection between records.","Associating a lookup or master-detail relationship between two objects.","Enabling the 'One-to-Many' option in the object settings."]];
-const answers = [D,B,D,C,B,D,C];
+                 ["Creating a custom junction object to link two objects.","Using external objects to establish a connection between records.","Associating a lookup or master-detail relationship between two objects.","Enabling the 'One-to-Many' option in the object settings."]
+                 ["True","False"]];
+const answers = [D,B,D,C,B,D,C,A];
 
 document.querySelector("#numberOfQuestion").innerHTML = test.length;
 
