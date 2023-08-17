d54ce3aba12d3c8980497475d6b53b209fbea391
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-18 00:50:10 +0300

    Update testAdmin5.js

--- admin/testAdmin5.js
+++ admin/testAdmin5.js
@@ -14,7 +14,9 @@ var i=0, n=0, m=0;
 var wrongs = 0, corrects = 0;
 
 // QUESTIONS AND OPTIONS CONTENTS
-const test = ["Which of the following is used to set highlights panel?", "Which of the following is not a Dashboards component type?", "How many fields on the compact layout does the record highlights section in Lightning Experience use?",
+const test = ["Which of the following is used to set highlights panel?", "Which of the following is not a dashboard component type?",
+              '"Opportunity Close Date must not be a date before today's date or a date within the next 1 month." <br> Which validation rule will ensure this situation?',
+              "How many fields on the compact layout does the record highlights section in Lightning Experience use?",
               "We want to automate assigning owners to Leads using the Assignment Rule. Which of the following can we assign?", "Which of the following field types do compact layouts support?"];
 
 const options1 = [["Compact Layout","Page Layout","Search Layout","List View Layout"], ["Chart","Gauge","Metric","Tabular"], [3,5,7,10], ["A user","A queue","A public group","A and B"], ["Phone","Text Area","Multi-select Picklist","Rich Text Area"]];
