21415258bc0ec75c2b4ba3842faa7a8db9b09f4b
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-22 02:39:57 +0300

    Update testDev4.js

--- developer/testDev4.js
+++ developer/testDev4.js
@@ -14,17 +14,18 @@ var i=0, n=0, m=0;
 var wrongs = 0, corrects = 0;
 
 // QUESTIONS AND OPTIONS CONTENTS
-const test = ["Which of the following is true for Apex?", "Which of the following is a fundamental principle of Object-Oriented Programming (OOP) in Apex?",
+const test = ["Which of the following is true for Apex?", "<code>String str = 'hello world'; <br> String str2 = str.capitalize(); <br> System.debug(str2);</code> <br> What is the output of the code snippet above?",
+              "Which of the following is a fundamental principle of Object-Oriented Programming (OOP) in Apex?",
               'Which of the following query returns all Food__c records with values ​​of "Orange" and "Morello" in a multi-select picklist field named Fruits__c?',
               "A space agency uses Salesforce to manage their spacecraft and missions. Each spacecraft has various parameters and functions. To model the behavior and attributes of these spacecraft, which concept should the space agency implement?",
               "<code>List&ltUser&gt users = [SELECT Id, FIELDS(ALL) FROM User LIMIT 200]; <br> System.debug(users); </code> <br> What is the output of the code snippet above?"];
 
 const options1 = [["Apex is a strongly typed, object-oriented programming language.","Apex uses syntax that looks like Java.","Apex enables developers to add business logic to most system events.","All of above."],
-                  ["SQL Query Optimization","Abstraction","HTML Coding","File Input/Output"],
+                  ["hello world","Hello world","Hello World","HELLO WORLD"], ["SQL Query Optimization","Abstraction","HTML Coding","File Input/Output"],
                   ["<code>[SELECT Id FROM Food__c WHERE Fruits__c = 'Orange,Morello']</code>","<code>[SELECT Id FROM Food__c WHERE Fruits__c IN 'Orange,Morello']</code>","<code>[SELECT Id FROM Food__c WHERE Fruits__c = 'Orange;Morello']</code>","<code>[SELECT Id FROM Food__c WHERE Fruits__c = 'Orange' AND Fruits__c = 'Morello']</code>"],
                   ["Classes and objects","Automation triggers","Standard Controllers","Markup Templates"],
                   ["Displays users with all field values.","Displays users with all standard field values.","Displays users with all custom field values.","Gives a duplicate field error."]];
-const answers = [D,B,C,A,D];
+const answers = [D,B,B,C,A,D];
 
 document.querySelector("#numberOfQuestion").innerHTML = test.length;
 
