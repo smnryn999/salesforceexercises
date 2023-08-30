635ed0f85ba930f649478e5e05f6255071e7087a
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-08-31 02:03:34 +0300

    Update testDev5.js

--- developer/testDev5.js
+++ developer/testDev5.js
@@ -14,14 +14,15 @@ var i=0, n=0, m=0;
 var wrongs = 0, corrects = 0;
 
 // QUESTIONS AND OPTIONS CONTENTS
-const test = ["batch class template, send notification, lightning-datatable?", "In a company called Página Blanca Sales Inc., Rose, Salesforce Developer, need to update 500 thousands opportunity records. Which code snippet should she use?", "Which of the following is true?", "In a Lightning Web Component (LWC) parent-child relationship, what is the purpose of the @api decorator in the child component?"];
+const test = ["batch class template, send notification, lightning-datatable?", "In a company called Página Blanca Sales Inc., Rose, Salesforce Developer, need to update 500 thousands opportunity records. Which code snippet should she use?", "Which of the following is true?", "In a Lightning Web Component (LWC) parent-child relationship, what is the purpose of the @api decorator in the child component?",
+             "<code>@RestResource(urlMapping='/Account/*') <br> global with sharing class MyRestResource { <br> &nbsp; @HttpDelete <br> &nbsp; global static void doDelete() { <br> &nbsp; &nbsp; RestRequest req = RestContext.request; <br> &nbsp; &nbsp; RestResponse res = RestContext.response; <br> &nbsp; &nbsp; String accountId = req.requestURI.substring(req.requestURI.lastIndexOf('/')+1); <br> &nbsp; &nbsp; Account account = [SELECT Id FROM Account WHERE Id = :accountId]; <br> &nbsp; &nbsp; delete account; <br> &nbsp; }<br>}</code>"];
 
 const options1 = [["","","",""],
                   ["<code>public class OpportunityBatch implements Database.Batchable&lt;sObject&gt; { <br> &nbsp; public Database.QueryLocator start(Database.BatchableContext bc) {<br> &nbsp; &nbsp; // Start method logic <br> &nbsp; } <br> &nbsp; public void execute(Database.BatchableContext bc, List&lt;Opportunity&gt; scope) { <br> &nbsp; &nbsp; // Execute method logic <br> &nbsp; } <br> &nbsp; public void finish(Database.BatchableContext bc) { <br> &nbsp; &nbsp; // Finish method logic <br> &nbsp; }<br>}</code>",
                    "<code>public class OpportunityBatch implements Database.Batchable&lt;sObject&gt; { <br> &nbsp; public void execute(Database.BatchableContext bc, List&lt;Opportunity&gt; scope) { <br> &nbsp; &nbsp; // Execute method logic <br> &nbsp; }<br>}</code>","<code>public class OpportunityQueue implements Queueable { <br> &nbsp; public void execute(QueueableContext qc) { <br> &nbsp; &nbsp; // Execute method logic <br> &nbsp; } <br>}</code>","<code>public class OpportunityClass { <br> &nbsp; public static void updateOpportunities(List&lt;Opportunity&gt; listOpp) { <br> &nbsp; &nbsp; // Execute method logic <br> &nbsp; } <br>}</code>"],
                   ["Abstract classes have a constructor method and can have instances.","Abstract classes have a constructor method but cannot have instances.","Abstract classes have not a constructor method but can have instances.","Abstract classes have not a constructor method and cannot have instances."],
-                  ["It specifies that parent component should be hidden from child components.","It restricts data binding between parent and child components.","It defines a public property that can be accessed by the parent component.","It controls the styling of the child component's template."]];
-const answers = [A,A,B,C];
+                  ["It specifies that parent component should be hidden from child components.","It restricts data binding between parent and child components.","It defines a public property that can be accessed by the parent component.","It controls the styling of the child component's template."], ["","","",""]];
+const answers = [A,A,B,C,C];
 
 document.querySelector("#numberOfQuestion").innerHTML = test.length;
 
