5bb6817a9e88f1cd1b64bb16daba9fef2b2ead31
Osman A <96185425+smnryn999@users.noreply.github.com>
2023-10-19 16:21:49 +0300

    Update examples.js

--- developer - examples/examples.js
+++ developer - examples/examples.js
@@ -78,7 +78,7 @@ var showButtons = document.querySelectorAll(".showSolution");
          content = content.replaceAll(s, "<span style='color:#EFFD5F'>"+s+"</span>");
        }
 
-      var logic = ["&&", "||", " !"];
+      var logic = ["&&", "||", " !", "console.error"];
       for(l of logic){
          content = content.replaceAll(l, "<span style='color:red'>"+l+"</span>");
        }
